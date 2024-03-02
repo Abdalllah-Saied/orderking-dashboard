cfebd6d83f48e57f73baa57020c104ef8bb264b8
abdullah <abdallasaid201@gmail.com>
2024-03-02 19:28:32 +0200

    edit register to add domain id in register user

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -40,6 +40,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'tenant_id' => config('tenant.id')??1,
         ]);
 
         event(new Registered($user));
